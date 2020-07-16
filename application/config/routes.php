dc0295def57bd76aa61cd520c4fd2c79e31ecac4
MuhammadSuryono1997 <msuryono0@gmail.com>
2020-07-17 01:54:35 +0900

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,10 +3,12 @@ error_reporting(0);
 use App\Controllers\C_users;
 $uri = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
+// var_dump($uri);
 
 $user = new C_users();
 
-if($uri[2] == "login")
+if($uri[3] == "login")
 {
+    $view = 'login';
     include(__DIR__.'/../view/index.php');
 }
--- application/view/index.php
+++ application/view/index.php
@@ -14,8 +14,7 @@
 </head>
 
 <body>
-
-    <?=__DIR__?>
+    <?php include __DIR__.'/'.$view.".php";?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
