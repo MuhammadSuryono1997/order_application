6e081acc68b08801d58055b67ee13b2d37bfd0c5
Iman Fauzi <imanfauzi29@gmail.com>
2020-07-16 22:32:43 +0700

    UI

---
+++ application/view/dashboard.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css.css"> -->
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <!-- <div class="container-fluid">
+        <div class="row bg-primary">
+            <div class="col-12 justify-content-between d-flex py-1">
+                <h2 class="text-light">Order</h2>
+                <button type="button" class="btn btn-success"><i class="fa fa-plus" aria-hidden="true"></i>
+                    Add Items</button>
+            </div>
+        </div>
+    </div> -->
+
+    <div class="container">
+        <div class="row mt-3">
+            <div class="col-md-4">
+                <div class="card" style="width: 16rem; min-height: 15rem; box-shadow: 0 7px 12px #888;">
+                    <div class="card-body px-2">
+                        <img class="mx-auto d-block" src="https://img.icons8.com/cotton/128/000000/user-male--v1.png" />
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-info btn-xs btn-block">User</button>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 16rem; min-height: 15rem; box-shadow: 0 7px 12px #888;">
+                    <div class="card-body px-2">
+                        <img class="mx-auto d-block" src="https://img.icons8.com/cute-clipart/64/000000/purchase-order.png" />
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-info btn-xs btn-block">Orders</button>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 16rem; min-height: 15rem; box-shadow: 0 7px 12px #888;">
+                    <div class="card-body px-2">
+                        <img class="mx-auto d-block" src="https://img.icons8.com/dusk/64/000000/list.png" />
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-info btn-xs btn-block">Items</button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
